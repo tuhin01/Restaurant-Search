4040f220e56a93b10755edafc7882a8774dc81e8
Tousif Rahman <tuhin.world@gmail.com>
2020-02-17 09:03:14 +0600

    Add Business Screen

---
+++ src/screens/BusinessScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {StyleSheet, Text, View} from "react-native";
+
+const BusinessScreen = () => {
+    return (
+        <View>
+            <Text>Hello business</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default BusinessScreen;
