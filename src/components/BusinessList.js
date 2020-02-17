9655fb1ee9c9440a41916193da7d5da6608c3284
Tousif Rahman <tuhin.world@gmail.com>
2020-02-17 10:17:44 +0600

    Hide category title if there is no restaurant

--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import {FlatList, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {useNavigation} from "@react-navigation/native";
 import BusinessDetails from "./BusinessDetails";
 
-const BusinessList = ({ title, businesses }) => {
+const BusinessList = ({title, businesses}) => {
+    if (!businesses.length) {
+        return null;
+    }
+
     const navigation = useNavigation();
     return (
         <View style={styles.mainContainer}>
