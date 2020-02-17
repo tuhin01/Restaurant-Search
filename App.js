7e71644f1154a224d0fe7cc757e6910ecc5961f8
Tousif Rahman <tuhin.world@gmail.com>
2020-02-17 09:03:37 +0600

    Update header style

--- App.js
+++ App.js
@@ -3,14 +3,28 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import SearchScreen from "./src/screens/SearchScreen";
+import BusinessScreen from "./src/screens/BusinessScreen";
 
 const Stack = createStackNavigator();
-
+const headerStyle = {
+    headerStyle: {
+        backgroundColor: "#f4511e",
+    },
+    headerTintColor: "#fff",
+    headerTitleStyle: {
+        fontWeight: "bold",
+    },
+};
 export default () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Search">
-                <Stack.Screen name="Search" component={SearchScreen} />
+            <Stack.Navigator initialRouteName="SearchScreen">
+                <Stack.Screen name="SearchScreen" component={SearchScreen} options={headerStyle} />
+                <Stack.Screen
+                    name="BusinessScreen"
+                    component={BusinessScreen}
+                    options={headerStyle}
+                />
             </Stack.Navigator>
         </NavigationContainer>
     );
